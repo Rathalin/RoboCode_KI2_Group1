fa97855f9cb5cf78e2e2c59bbcff9763ab8d3983
Rathalin <danielflo1997@hotmail.com>
2022-04-15 16:03:47 +0200

    Add example robot

---
+++ src/at/fhooe/ai/im/g1/MyFirstRobot.java
@@ -0,0 +1,12 @@
+package at.fhooe.ai.im.g1;
+
+import robocode.AdvancedRobot;
+
+public class MyFirstRobot extends AdvancedRobot {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+}
